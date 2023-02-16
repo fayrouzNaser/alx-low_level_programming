38ba928d366a30a863e26d47f7655cdcaa0a771e
fayrouzNaser <fayrouznaser20@gmail.com>
2023-02-16 15:45:43 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stadio.h>
-/**                                                                                                                                          * main - Entry point
-*
-*  Return: Always 0 (Success)
-*/
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
